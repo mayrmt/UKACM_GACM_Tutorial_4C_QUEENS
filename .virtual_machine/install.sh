3869e8b81aac86b0b992c5494abe2c1481fc0d8e
Matthias Mayr <matthias.mayr@unibw.de>
2025-09-25 23:44:41 +0200

    Add sudo

--- .virtual_machine/install.sh
+++ .virtual_machine/install.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y locales && \
-    localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8 && \
-    locale-gen en_US.UTF-8 && \
-    rm -rf /var/lib/apt/lists/*
 
-apt-get update && apt-get install -y \
+sudo apt-get update
+sudo apt-get install -y \
       build-essential \
       ffmpeg \
       git \
       libglu1-mesa \
-      python3 \
-      sudo \
       unzip \
       vim \
       wget \
-      && \
-    apt-get update && apt-get install -y \
-      doxygen \
-      graphviz \
-      texinfo \
-      lcov \
       libblas-dev \
       libboost-all-dev \
       libcln-dev \
@@ -43,5 +32,6 @@ apt-get update && apt-get install -y \
       clang-tidy \
       clang-tools \
       libomp-dev \
-      libvtk9-dev \
-  && rm -rf /var/lib/apt/lists/*
+      libvtk9-dev
+
+#  && rm -rf /var/lib/apt/lists/*

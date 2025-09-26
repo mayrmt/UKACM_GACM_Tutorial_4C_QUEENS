f78a2d4570a9a4fdb44b8b8fab3199929c042cc0
Matthias Mayr <matthias.mayr@unibw.de>
2025-09-26 11:03:31 +0200

    More steps towards arm64

---
+++ .virtual_machine/README.md
@@ -0,0 +1,17 @@
+# Installation of 4C & QUEENS dependencies
+
+## Prerequesites
+
+A fresh installation of a Ubuntu 24.04 linux box
+
+## Customization
+
+Installations paths are hard-coded in the scripts. To change them, adapt the variable `INSTALL_DIR` in `install.sh`.
+
+## Instructions
+
+Execute the installation script:
+
+```bash
+./install.sh
+```
--- .virtual_machine/cmake/install.sh
+++ .virtual_machine/cmake/install.sh
@@ -11,21 +11,9 @@ INSTALL_DIR="$1"
 # Number of procs for building (default 4)
 NPROCS=${NPROCS=4}
 VERSION="3.30.5"
-CHECKSUM="83de8839f3fb0d9caf982a0435da3fa8c4fbe2c817dfec99def310dc7e6a8404"
 
-wget --no-verbose https://github.com/Kitware/CMake/releases/download/v${VERSION}/cmake-${VERSION}-linux-x86_64.sh
-# Verify checksum
-if [ $CHECKSUM = `sha256sum cmake-${VERSION}-linux-x86_64.sh | awk '{print $1}'` ]
-then
+wget --no-verbose https://cmake.org/files/v3.30/cmake-3.30.5-linux-aarch64.sh
 
-  echo "Checksum matches"
-else
-  sha256sum cmake-${VERSION}-linux-x86_64.sh | awk '{print $1}'
-  echo $CHECKSUM
-  echo "Checksum does not match"
-  exit 1
-fi
-
-chmod +x cmake-${VERSION}-linux-x86_64.sh
-./cmake-${VERSION}-linux-x86_64.sh --prefix=${INSTALL_DIR} --skip-license
-rm cmake-${VERSION}-linux-x86_64.sh
+chmod +x cmake-3.30.5-linux-aarch64.sh
+./cmake-3.30.5-linux-aarch64.sh --prefix=${INSTALL_DIR} --skip-license
+rm cmake-3.30.5-linux-aarch64.sh
